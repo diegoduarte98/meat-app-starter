06b760b30cf31c72950ad3f7a6f588119cfb1133
Diego Duarte <diegoduarte98@gmail.com>
2018-11-27 00:16:06 -0200

    menu - menu-item e shopping-cart

---
+++ src/app/detail-restaurants/detail-restaurants.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-detail-restaurants',
+  templateUrl: './detail-restaurants.component.html'
+})
+export class DetailRestaurantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
